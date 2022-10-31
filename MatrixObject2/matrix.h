53105cda5a7d4ebdb021d4a29b265547acaa24aa
Richard Clarke <richard@clarke.biz>
2022-10-31 16:53:13 +1300

    Update, end of day 2 exercises

---
+++ MatrixObject2/matrix.h
@@ -0,0 +1,63 @@
+#include <string>
+
+using namespace std;
+
+class Object
+{
+public:
+    Object()
+    {
+    }
+    void trace()
+    {
+        cout << "Here";
+    }
+    
+    /*To be overriddeen by derived classes, so mark this as a virtual function to 
+     * ensure correct method resolution
+     */
+    virtual string nameOf()
+    {
+        string s;
+        s = "Object";
+        return s;
+    }
+};
+
+#if 0
+class Matrix : public Object
+{
+public:
+    Matrix();
+    
+    explicit Matrix(int initial_value);
+    Matrix(const Matrix& matrix);
+    ~Matrix()
+    {
+        --m_count;
+    }
+    string nameOf();
+    void setElement(unsigned row, unsigned col, int value = 0);
+    void init(int val);
+    /*Zero the fixed size matrix*/
+    void empty();
+    
+    bool operator==(const Matrix& othermatrix);
+    bool operator!=(const Matrix& othermatrix);
+    Matrix operator+ (const Matrix& othermatrix) const;
+    Matrix operator- (const Matrix& othermatrix) const;
+    Matrix& operator+=(const Matrix& othermatrix);
+    int operator[](unsigned index) const;
+    int& operator()(unsigned row, unsigned col) ;
+    const int& operator()(unsigned row, unsigned col) const;
+    
+    friend ostream& operator<<(ostream& os, const Matrix& m); 
+private:
+    static const unsigned MAX_ROW = 2;
+    static const unsigned MAX_COL = 3;
+    static int m_count; //keep track of number of instances of this class
+    array<array<int,MAX_COL>,MAX_ROW> m_matrix;
+    mutable unsigned m_print_count; //this can be updated even for const Matrix objects
+    
+};
+#endif
---
+++ MatrixThrow1/main.cpp
@@ -0,0 +1,255 @@
+/*
+ *	MatrixMain1.cpp
+ *		Program to be completed by students.
+ */
+#include <iostream>
+#include <ostream>
+#include <array>
+#include <exception>
+#include "matrix.h"
+
+
+Matrix::Matrix()
+{
+    ++m_count; //Keep track of the number of instances of this class that have been instantiated.
+    m_print_count = 0;
+    //std::cout << "Creating instance " << m_count << std::endl;
+    empty();
+}
+
+Matrix::Matrix(int initial_value)
+{
+    /*Keep track of the number of instances of this class that have been instantiated. Have to
+     * do this increment in every constructor type*/
+    ++m_count;
+    m_print_count = 0;
+    //std::cout << "Creating with value " << initial_value << " instance " << m_count << std::endl;
+    init(initial_value);
+}
+
+/*Copy constructor*/
+Matrix::Matrix(const Matrix& matrix)
+{
+    ++m_count;
+    m_print_count = 0;
+    //std::cout << "Creating via copy , instance " << m_count << std::endl;
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            this->m_matrix[j][k] = matrix.m_matrix[j][k];
+        }
+    }
+    
+}
+
+string Matrix::nameOf()
+{
+    string s;
+    s = "Matrix";
+    return s;
+}
+
+/*Assign values to each element of the fixed sized matrix*/
+void Matrix::setElement(unsigned row, unsigned col, int value)
+{
+    /*Range checking is automatic with std::array type*/
+    if(row >= MAX_ROW or col >=MAX_COL)
+    {
+        throw ARRAY_BOUND_ERR;
+    }
+    m_matrix[row][col] = value;
+} 
+
+
+void Matrix::init(int val)
+{
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            m_matrix[j][k] = val;
+        }
+    }
+}
+
+/*Zero the fixed size matrix*/
+void Matrix::empty()
+{
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            m_matrix[j][k] = 0;
+        }
+    }
+}
+
+bool Matrix::operator==(const Matrix& othermatrix)
+{
+   bool result = true;
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            if (this->m_matrix[j][k] != othermatrix.m_matrix[j][k])
+            {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+bool Matrix::operator!=(const Matrix& othermatrix)
+{
+    return (!(*this == othermatrix));
+}
+
+Matrix Matrix::operator+ (const Matrix& othermatrix) const
+{
+    Matrix result;
+    
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            result.m_matrix[j][k] = this->m_matrix[j][k] + othermatrix.m_matrix[j][k];
+        }
+    }
+    
+    return result;
+}
+
+/*Overload the subtraction operator for the Matrix class*/
+Matrix Matrix::operator- (const Matrix& othermatrix) const
+{
+    Matrix result;
+    
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            result.m_matrix[j][k] = this->m_matrix[j][k] - othermatrix.m_matrix[j][k];
+        }
+    }
+    
+    return result;
+}
+
+/*Overload the in-place operator += (Dyadic, or binary) */
+Matrix& Matrix::operator+=(const Matrix& othermatrix)
+{
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            this->m_matrix[j][k] = this->m_matrix[j][k] + othermatrix.m_matrix[j][k];
+        }
+    }
+    return *this;
+}
+
+/*Overload the monadic operator + */
+/*TODO*/
+
+/*Overload the operator [],
+ * treating rows and columns as single long row*/
+int Matrix::operator[](unsigned index) const
+{
+    unsigned temp = 0;
+    for(size_t j=0; j< MAX_ROW; j++)
+    {
+        for(size_t k=0; k< MAX_COL; k++)
+        {
+            
+            if(temp++ == index)
+            {
+                return m_matrix[j][k];
+            }
+        }
+    }
+    
+    return 0;
+}
+
+
+/*Useful for cleaner assignment of values to the matrix object. Returning a reference to the int element
+ * to allow something to be assigned to it*/
+int& Matrix::operator()(unsigned row, unsigned col) 
+{
+    return m_matrix.at(row).at(col);
+}
+
+const int& Matrix::operator()(unsigned row, unsigned col) const
+{
+    if(row > (MAX_ROW-1) or col > (MAX_COL-1))
+    {
+        cout << "Exceeds matrix dimension, setting to MAX_ROW, MAX_COL" << std::endl;
+        row = MAX_ROW;
+        col = MAX_COL;
+    }
+    return m_matrix.at(row).at(col);
+}
+
+
+
+
+ostream& operator<<(ostream& os, const Matrix& m)
+{
+    ++m.m_print_count;
+    os << "[" ;
+    for(size_t j=0; j< Matrix::MAX_ROW; j++)
+    {
+        os << "[";
+        size_t k;
+        for(k=0; k < Matrix::MAX_COL-1; k++)
+        {
+            os << m.m_matrix[j][k] << ", ";
+        }
+        os << m.m_matrix[j][k] << "]";
+    }
+    os << "]" << std::endl;
+    //os << "printed " << m.m_print_count << " times" << std::endl << std::endl;
+    return os;
+}
+
+
+int Matrix::m_count = 0;
+int main()
+{
+    
+	Matrix m1;
+    
+    m1(0,0) = 10;
+    m1(0,1) = 20;
+    
+    try
+    {
+        m1.setElement(3,4, 24); // Attempting to set an element outside the bounds of the array
+    }
+    catch(int err)
+    {
+        cout << "Caught array bound exception " << err << endl;
+    }
+    catch(exception& e) //std library throws exceptions of this type
+    {
+        cout << "Exception: " << e.what();
+    }
+    
+    cout << "m1 is : " << m1;
+    
+    cout << "m1 is of type: " << m1.nameOf() << endl;
+    m1.trace();
+    
+    /*Object o;
+    cout << "o is type: " << o.nameOf() << endl;
+    o.trace();
+    cout << endl;
+     * */
+     cout << "From C++ version " << __cplusplus << endl;
+     
+    
+	return 0;
+}
